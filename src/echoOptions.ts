263b4e1804495d96452cb5ad58ea7d68a0b87c7d
yoyo930021 <yoyo930021@gmail.com>
2018-11-05 15:48:15 +0800

    Fix EchoOptions have default

--- src/echoOptions.ts
+++ src/echoOptions.ts
@@ -1,12 +1,12 @@
 import { PusherStatic, AuthConfig } from "pusher-js";
 
 export default interface EchoOptions extends PusherStatic, SocketIOClient.ConnectOpts {
-    namespace: string,
-    authEndpoint: string,
-    broadcaster: string,
-    csrfToken: string | null,
-    host: string | undefined,
-    key: string | undefined,
+    namespace?: string,
+    authEndpoint?: string,
+    broadcaster?: string,
+    csrfToken?: string | null,
+    host?: string | undefined,
+    key?: string | undefined,
     client?: Pusher.Pusher | SocketIOClient.Socket,
-    auth: AuthConfig
+    auth?: AuthConfig
 }
