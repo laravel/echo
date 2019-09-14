53540bde42bedc85a539491d0f95ab4891388617
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-09-14 10:45:07 -0300

    chore(package: sort, update deps and split jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'json',
+        'node'
+    ]
+};
