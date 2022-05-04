376cce4f2ce9d12cfd34c1b720502cbba43c88f8
Owen Pearson <owen.pearson@ably.com>
2022-05-05 00:55:00 +0100

    Type Ably as any

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,5 @@
-import { Types as AblyTypes } from 'ably';
-
 declare let Pusher: any;
-declare let Ably: { Realtime: AblyTypes.RealtimeCallbacks };
+declare let Ably: any;
 declare let io: any;
 declare let Vue: any;
 declare let axios: any;
