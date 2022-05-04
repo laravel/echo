bb6c747c9291e948b37a6852ba308ed2280ead36
Owen Pearson <owen.pearson@ably.com>
2022-05-05 00:51:44 +0100

    Add typing to interact with window.Ably

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,7 @@
+import { Types as AblyTypes } from 'ably';
+
 declare let Pusher: any;
+declare let Ably: { Realtime: AblyTypes.RealtimeCallbacks };
 declare let io: any;
 declare let Vue: any;
 declare let axios: any;
