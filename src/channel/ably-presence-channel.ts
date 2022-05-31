322f81b827a1755423fde210c2f10982ed453817
sacOO7 <sachinshinde7676@gmail.com>
2022-05-31 20:49:09 +0530

    Updated presence channel, reformatted code

--- src/channel/ably-presence-channel.ts
+++ src/channel/ably-presence-channel.ts
@@ -5,62 +5,86 @@ import { PresenceChannel } from './presence-channel';
  * This class represents an Ably presence channel.
  */
 export class AblyPresenceChannel extends AblyChannel implements PresenceChannel {
-    /**
-     * Register a callback to be called anytime the member list changes.
-     */
-    here(callback: Function): AblyPresenceChannel {
-        this.channel.presence.get((err, members) => {
-          if(err) {
-            callback(err);
-            return;
-          }
+  /**
+   * Register a callback to be called anytime the member list changes.
+   */
+  here(callback: Function): AblyPresenceChannel {
+    this.channel.presence.get((err, members) => {
+      if (err) {
+        callback(null, err);
+        return;
+      }
 
-          callback(members);
+      callback(members, null);
+    })
 
-        })
+    return this;
+  }
 
-        return this;
-    }
+  /**
+   * Listen for someone joining the channel.
+   */
+  joining(callback: Function): AblyPresenceChannel {
+    this.channel.presence.subscribe('enter', (member) => {
+      callback(member);
+    });
 
-    /**
-     * Listen for someone joining the channel.
-     */
-    joining(callback: Function): AblyPresenceChannel {
-        this.channel.presence.subscribe('enter', (member) => {
-            callback(member);
-        });
+    return this;
+  }
 
-        return this;
-    }
+  /**
+   * Listen for someone leaving the channel.
+   */
+  leaving(callback: Function): AblyPresenceChannel {
+    this.channel.presence.subscribe('leave', (member) => {
+      callback(member);
+    });
 
-    /**
-     * Listen for someone leaving the channel.
-     */
-    leaving(callback: Function): AblyPresenceChannel {
-        this.channel.presence.subscribe('leave', (member) => {
-            callback(member);
-        });
+    return this;
+  }
 
-        return this;
-    }
+  /**
+   * Enter presence
+   * @param data - Data to be published while entering the channel
+   * @param callback - success/error callback (err) => {}
+   * @returns AblyPresenceChannel
+   */
+  enter(data: any, callback: Function): AblyPresenceChannel {
+    this.channel.presence.enter(data, callback as any);
+    return this;
+  }
 
-    // TODO - Need to check for whisper implementation for presence channels
-    /**
-     * Trigger client event on the channel.
-     */
-    whisper(eventName: string, data: any): AblyPresenceChannel {
-        switch (eventName) {
-          case 'enter':
-            this.channel.presence.enter(data);
-            break;
-          case 'leave':
-            this.channel.presence.leave(data);
-            break;
-          case 'update':
-            this.channel.presence.update(data);
-            break;
-        }
+  /**
+   * Leave presence
+   * @param data - Data to be published while leaving the channel
+   * @param callback - success/error callback (err) => {}
+   * @returns AblyPresenceChannel
+   */
+  leave(data: any, callback: Function): AblyPresenceChannel {
+    this.channel.presence.leave(data, callback as any);
 
-        return this;
-    }
+    return this;
+  }
+
+  /**
+ * Update presence
+ * @param data - Update presence with data
+ * @param callback - success/error callback (err) => {}
+ * @returns AblyPresenceChannel
+ */
+  update(data: any, callback: Function): AblyPresenceChannel {
+    this.channel.presence.update(data, callback as any);
+
+    return this;
+  }
+
+  /**
+ * Trigger client event on the channel.
+ */
+  whisper(eventName: string, data: any): AblyPresenceChannel {
+    this.channel.publish(`client-${eventName}`, data);
+
+    return this;
+  }
+
 }
