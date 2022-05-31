b88bb016d3399ddea832d8e04caf21c78e1ed5a8
sacOO7 <sachinshinde7676@gmail.com>
2022-05-31 20:20:56 +0530

    refactored ablyconnector, removed unnecessary encrypted channel

--- src/connector/ably-connector.ts
+++ src/connector/ably-connector.ts
@@ -1,7 +1,7 @@
 import { Connector } from './connector';
 import { AblyChannel } from '../channel/ably-channel';
+import { AblyPrivateChannel } from '../channel/ably-private-channel';
 import * as AblyImport from 'ably';
-import { PresenceChannel } from '../channel';
 import { AblyPresenceChannel } from '../channel/ably-presence-channel';
 
 /**
@@ -29,56 +29,47 @@ export class AblyConnector extends Connector {
         }
     }
 
-    _channel(name: string): AblyChannel {
-        if (!this.channels[name]) {
-            this.channels[name] = new AblyChannel(this.ably, name, this.options);
-        }
-
-        return this.channels[name];
-    }
-
     /**
      * Listen for an event on a channel instance.
      */
     listen(name: string, event: string, callback: Function): AblyChannel {
-        return this._channel(name).listen(event, callback);
+        return this.channel(name).listen(event, callback);
     }
 
     /**
      * Get a channel instance by name.
      */
     channel(name: string): AblyChannel {
-        return this._channel(`public:${name}`);
+        const prefixedName = `public:${name}`; // adding public as a ably namespace prefix
+        if (!this.channels[prefixedName]) {
+            this.channels[prefixedName] = new AblyChannel(this.ably, name, this.options);
+        }
+
+        return this.channels[prefixedName];
     }
 
     /**
      * Get a private channel instance by name.
      */
-    privateChannel(name: string): AblyChannel {
-        return this._channel(`private:${name}`);
+    privateChannel(name: string): AblyPrivateChannel {
+        const prefixedName = `private:${name}`; // adding private as a ably namespace prefix
+        if (!this.channels[prefixedName]) {
+            this.channels[prefixedName] = new AblyPrivateChannel(this.ably, name, this.options);
+        }
+
+        return this.channels[prefixedName] as AblyPrivateChannel;
     }
 
     /**
     * Get a presence channel instance by name.
     */
-    presenceChannel(name: string): PresenceChannel {
-        return this._channel(`presence:${name}`) as AblyPresenceChannel;
-    }
+    presenceChannel(name: string): AblyPresenceChannel {
+        const prefixedName = `presence:${name}`; // adding presence as a ably namespace prefix
+        if (!this.channels[prefixedName]) {
+            this.channels[prefixedName] = new AblyPresenceChannel(this.ably, name, this.options);
+        }
 
-    /**
-     * Get a private encrypted channel instance by name.
-     */
-    encryptedPrivateChannel(name: string): AblyChannel {
-        // if (!this.channels['private-encrypted-' + name]) {
-        //     this.channels['private-encrypted-' + name] = new AblyEncryptedPrivateChannel(
-        //         this.ably,
-        //         'private-encrypted-' + name,
-        //         this.options
-        //     );
-        // }
-        //
-        // return this.channels['private-encrypted-' + name];
-        return this.channel(name);
+        return this.channels[prefixedName] as AblyPresenceChannel;
     }
 
     /**
