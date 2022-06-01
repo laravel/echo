6bd8b79d5284a1ae4fa537adbb73d12997ffdc28
sacOO7 <sachinshinde7676@gmail.com>
2022-06-01 23:46:23 +0530

    Added ably channel and connection test

---
+++ tests/ably/ably-channel.test.ts
@@ -0,0 +1,47 @@
+import { setup, tearDown } from './setup/sandbox';
+import * as Ably from 'ably';
+
+jest.setTimeout(20000);
+describe('AblyChannel', () => {
+    let testApp;
+
+    beforeAll(done => {
+        setup((err, app) => {
+            if (err) {
+                done(err);
+                return;
+            }
+            testApp = app;
+            done();
+        })
+    })
+
+    test('init with key string', () => {
+        const apiKey = testApp.keys[0].keyStr;
+        expect(typeof apiKey).toBe('string');
+        expect(apiKey).toBeTruthy();
+    })
+
+    test('rest time should work', (done) => {
+        const apiKey = testApp.keys[0].keyStr;
+        const restClient = new Ably.Rest(apiKey);
+        restClient.time((err, time) => {
+            if (err) {
+                done(err);
+                return;
+            }
+            expect(typeof time).toBe('number');
+            done()
+        });
+    })
+
+    afterAll((done) => {
+        tearDown(testApp, (err) => {
+            if (err) {
+                done(err);
+                return;
+            }
+            done();
+        })
+    })
+});
---
+++ tests/ably/ably-connection.test.ts
@@ -0,0 +1,52 @@
+import { setup, tearDown } from './setup/sandbox';
+import Echo from '../../src/echo';
+import { MockAuthServer } from './setup/mock-auth-server';
+
+jest.setTimeout(20000);
+describe('AblyConnection', () => {
+    let testApp;
+    let mockAuthServer;
+
+    beforeAll(done => {
+        console.log('before all called');
+        setup((err, app) => {
+            if (err) {
+                done(err);
+                return;
+            }
+            testApp = app;
+            console.log(testApp);
+            mockAuthServer = new MockAuthServer(testApp.keys[0].keyStr);
+            done();
+        })
+    })
+
+    test('should be able to connect to server', (done) => {
+        const echo = new Echo({
+            broadcaster: 'ably',
+            useTls: true,
+            environment: 'sandbox',
+            requestTokenFn: mockAuthServer.getSignedToken
+        });
+        echo.connector.ably.connection.on(({ current, reason }) => {
+            console.log(current, reason);
+            if (current == 'connected') {
+                done();
+            }
+            if (reason) {
+                done(reason)
+            }
+        });
+    })
+
+    afterAll((done) => {
+        console.log('after all called');
+        tearDown(testApp, (err) => {
+            if (err) {
+                done(err);
+                return;
+            }
+            done();
+        })
+    })
+});
