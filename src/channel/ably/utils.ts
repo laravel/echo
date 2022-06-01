ad69c0ff2f8b84e6a40ec5e54ea60ca84526889a
sacOO7 <sachinshinde7676@gmail.com>
2022-06-01 11:16:45 +0530

    Refactored utils, added proper types

--- src/channel/ably/utils.ts
+++ src/channel/ably/utils.ts
@@ -2,35 +2,32 @@ export const isNullOrUndefined = (obj) => obj == null || obj === undefined;
 export let isEmptyString = (stringToCheck, ignoreSpaces = true) => (ignoreSpaces ? stringToCheck.trim() : stringToCheck) === '';
 export const isNullOrUndefinedOrEmpty = (obj) => obj == null || obj === undefined || isEmptyString(obj);
 
-export const parseJwt = (token) => {
-    try {
-        // Get Token Header
-        const base64HeaderUrl = token.split('.')[0];
-        const base64Header = base64HeaderUrl.replace('-', '+').replace('_', '/');
-        const headerData = JSON.parse(asciiToBase64(base64Header));
+/**
+ * @throws Exception if parsing error
+ */
+export const parseJwt = (jwtToken: string) => {
+    // Get Token Header
+    const base64HeaderUrl = jwtToken.split('.')[0];
+    const base64Header = base64HeaderUrl.replace('-', '+').replace('_', '/');
+    const headerData = JSON.parse(asciiToBase64(base64Header));
 
-        // Get Token payload and date's
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace('-', '+').replace('_', '/');
-        const dataJWT = JSON.parse(asciiToBase64(base64));
-        dataJWT.header = headerData;
+    // Get Token payload and date's
+    const base64Url = jwtToken.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    const dataJWT = JSON.parse(asciiToBase64(base64));
+    dataJWT.header = headerData;
 
-        // TODO: add expiration at check ...
-
-        return dataJWT;
-    } catch (err) {
-        return false;
-    }
+    return dataJWT;
 }
 
-export const toTokenDetails = (jwtTokenString) => {
-    const parsedJwt = parseJwt(jwtTokenString);
+export const toTokenDetails = (jwtToken: string) => {
+    const parsedJwt = parseJwt(jwtToken);
     return {
         capability: parsedJwt['x-ably-capability'],
         clientId: parsedJwt['x-ably-clientId'],
         expires: parsedJwt.exp * 1000, // Convert Seconds to ms
         issued: parsedJwt.iat * 1000,
-        token: jwtTokenString
+        token: jwtToken
     }
 }
 
