7cafe3cff54b87a1c127a60dfd123996b6c55f12
sacOO7 <sachinshinde7676@gmail.com>
2022-06-08 19:35:50 +0530

    Added ably-channel tests for listening to the event

--- tests/ably/ably-channel.test.ts
+++ tests/ably/ably-channel.test.ts
@@ -1,9 +1,14 @@
 import { setup, tearDown } from './setup/sandbox';
-import * as Ably from 'ably';
+import Echo from '../../src/echo';
+import { MockAuthServer } from './setup/mock-auth-server';
+import { AblyChannel } from '../../src/channel';
+import safeAssert from './setup/utils';
 
 jest.setTimeout(20000);
 describe('AblyChannel', () => {
-    let testApp;
+    let testApp: any;
+    let mockAuthServer: MockAuthServer;
+    let echo: Echo;
 
     beforeAll(done => {
         setup((err, app) => {
@@ -12,27 +17,9 @@ describe('AblyChannel', () => {
                 return;
             }
             testApp = app;
+            mockAuthServer = new MockAuthServer(testApp.keys[0].keyStr);
             done();
         })
-    })
-
-    test('init with key string', () => {
-        const apiKey = testApp.keys[0].keyStr;
-        expect(typeof apiKey).toBe('string');
-        expect(apiKey).toBeTruthy();
-    })
-
-    test('rest time should work', (done) => {
-        const apiKey = testApp.keys[0].keyStr;
-        const restClient = new Ably.Rest(apiKey);
-        restClient.time((err, time) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(typeof time).toBe('number');
-            done()
-        });
     })
 
     afterAll((done) => {
@@ -44,4 +31,61 @@ describe('AblyChannel', () => {
             done();
         })
     })
+
+    beforeEach(() => {
+        echo = new Echo({
+            broadcaster: 'ably',
+            useTls: true,
+            environment: 'sandbox',
+            requestTokenFn: mockAuthServer.getSignedToken
+        });
+    });
+
+    afterEach(() => {
+        echo.disconnect();
+    });
+
+    test('channel subscription', (done) => {
+        echo.channel('test').subscribed(() => {
+            done();
+        });
+    });
+
+    test('Listen for a default broadcaster event', (done) => {
+        const publicChannel = echo.channel('test') as AblyChannel;
+        publicChannel
+            .subscribed(() => {
+                mockAuthServer.broadcast('public:test', 'App\\Events\\testEvent', 'Hello there');
+            })
+            .listen('testEvent', data => {
+                safeAssert(() => expect(data).toBe('Hello there'), done);
+            });
+    });
+
+    // https://laravel.com/docs/9.x/broadcasting#broadcast-name
+    test('Listen for a broadcast as event', (done) => {
+        const publicChannel = echo.channel('test') as AblyChannel;
+        publicChannel
+            .subscribed(() => {
+                mockAuthServer.broadcast('public:test', 'testEvent', 'Hello there');
+            })
+            .listen('.testEvent', data => {
+                safeAssert(() => expect(data).toBe('Hello there'), done);
+            });
+    });
+
+    test('Listen for all events', (done) => {
+        const publicChannel = echo.channel('test1') as AblyChannel;
+        publicChannel
+            .subscribed(() => {
+                mockAuthServer.broadcast('public:test1', 'testEvent', 'Hello there');
+            })
+            .listenToAll((eventName, data) => {
+                safeAssert(() => {
+                    expect(eventName).toBe('.testEvent');
+                    expect(data).toBe('Hello there');
+                }, done);
+            });
+    })
+
 });
