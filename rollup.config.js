6a1db3e03057e2d763d03042b8ef1298cf97aced
Tim MacDonald <hello@timacdonald.me>
2022-06-29 14:35:55 +1000

    fix iife build

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,38 @@
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
-    input: './src/echo.ts',
-    output: [
-        { file: './dist/echo.js', format: 'esm' },
-        { file: './dist/echo.common.js', format: 'cjs' },
-        { file: './dist/echo.iife.js', format: 'iife', name: 'Echo' },
-    ],
-    plugins: [
-        typescript(),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            extensions: ['.ts'],
-            presets: ['@babel/preset-env'],
-            plugins: [
-                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                '@babel/plugin-proposal-function-sent',
-                '@babel/plugin-proposal-export-namespace-from',
-                '@babel/plugin-proposal-numeric-separator',
-                '@babel/plugin-proposal-throw-expressions',
-                '@babel/plugin-transform-object-assign',
-            ],
-        }),
-    ],
-};
+const plugins = [
+    typescript(),
+    babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        extensions: ['.ts'],
+        presets: ['@babel/preset-env'],
+        plugins: [
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+            '@babel/plugin-transform-object-assign',
+        ],
+    }),
+];
+
+export default [
+    {
+        input: './src/echo.ts',
+        output: [
+            { file: './dist/echo.js', format: 'esm' },
+            { file: './dist/echo.common.js', format: 'cjs' },
+        ],
+        plugins,
+    },
+    {
+        input: './src/index.iife.ts',
+        output: [
+            { file: './dist/echo.iife.js', format: 'iife', name: 'Echo' },
+        ],
+        plugins,
+    }
+];
