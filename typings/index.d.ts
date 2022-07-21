9e691f0b4c30000a806d22aacc8272cbf6bec6b1
sacOO7 <sachinshinde7676@gmail.com>
2022-07-21 16:12:35 +0530

    Added missing declarations for Vue, Axios and jQuery inside global

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,14 +1,11 @@
-declare let Pusher: any;
-declare let Ably: any;
-declare let io: any;
-declare let Vue: any;
-declare let axios: any;
-declare let jQuery: any;
-
-export {};
 // libs required to be set globally before initializing echo instance (currently used in tests)
 declare global {
     var Ably: any;
     var Pusher: any;
     var io: any;
+    var Vue: any;
+    var axios: any;
+    var jQuery: any;
 }
+
+export {};
