f7ef2d4f6af319fffba5685b2a019e58aac95018
sacOO7 <sachinshinde7676@gmail.com>
2022-07-22 01:49:56 +0530

    Removed unnecessary test-app-setup.json file, updated sandbox

--- tests/ably/setup/sandbox.ts
+++ tests/ably/setup/sandbox.ts
@@ -1,74 +1,39 @@
-import path from "path";
-import fs from 'fs';
 import { httpReqFunction } from "../../../src/channel/ably/utils";
 
 let restHost = 'sandbox-rest.ably.io';
 const tlsPort = 443;
 const toBase64 = (text: string) => Buffer.from(text, 'binary').toString('base64');
 
-const loadJsonData = (dataPath, callback) => {
-    fs.readFile(path.join(__dirname, dataPath), (err, data: Buffer) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        try {
-            data = JSON.parse(data.toString());
-        } catch (e) {
-            callback(e);
-            return;
-        }
-        callback(null, data);
-    });
-}
-
 const httpReq = httpReqFunction();
 
-function prefixDomainWithEnvironment(domain, environment) {
-    if (environment.toLowerCase() === 'production') {
-        return domain;
-    } else {
-        return environment + '-' + domain;
-    }
-}
-
 const creatNewApp = (callback) => {
-    loadJsonData('test-app-setup.json', function (err, testData) {
+    var postData = JSON.stringify({
+        "keys": [{}],
+        "namespaces": []
+    });
+    var postOptions = {
+        host: restHost,
+        port: tlsPort,
+        path: '/apps',
+        method: 'POST',
+        scheme: 'https',
+        headers: { Accept: 'application/json', 'Content-Type': 'application/json', 'Content-Length': postData.length },
+        body: postData,
+    };
+
+    httpReq(postOptions, function (err, res) {
         if (err) {
             callback(err);
-            return;
+        } else {
+            if (typeof res === 'string') res = JSON.parse(res);
+            var testApp = {
+                accountId: res.accountId,
+                appId: res.appId,
+                keys: res.keys,
+                cipherConfig: undefined
+            };
+            callback(null, testApp);
         }
-        var postData = JSON.stringify(testData.post_apps);
-        var postOptions = {
-            host: restHost,
-            port: tlsPort,
-            path: '/apps',
-            method: 'POST',
-            scheme: 'https',
-            headers: { Accept: 'application/json', 'Content-Type': 'application/json', 'Content-Length': postData.length },
-            body: postData,
-        };
-
-        httpReq(postOptions, function (err, res) {
-            if (err) {
-                callback(err);
-            } else {
-                if (typeof res === 'string') res = JSON.parse(res);
-                if (res.keys.length != testData.post_apps.keys.length) {
-                    callback('Failed to create correct number of keys for app');
-                } else if (res.namespaces.length != testData.post_apps.namespaces.length) {
-                    callback('Failed to create correct number of namespaces for app');
-                } else {
-                    var testApp = {
-                        accountId: res.accountId,
-                        appId: res.appId,
-                        keys: res.keys,
-                        cipherConfig: testData.cipher,
-                    };
-                    callback(null, testApp);
-                }
-            }
-        });
     });
 }
 
