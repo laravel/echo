39b3ba783c04911f8ed1028f4a1fc72d67feccd8
Ponleu <chhornponleu@users.noreply.github.com>
2022-10-19 20:55:26 +0700

    instantiate pusher using Pusher class from option (#359)
    * instantiate pusher using Pusher class from option.
    
    * Update pusher-connector.ts
    
    Co-authored-by: ponleu <ponleu@go24.sg>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/connector/pusher-connector.ts
+++ src/connector/pusher-connector.ts
@@ -27,6 +27,8 @@ export class PusherConnector extends Connector {
     connect(): void {
         if (typeof this.options.client !== 'undefined') {
             this.pusher = this.options.client;
+        } else if (this.options.Pusher) {
+            this.pusher = new this.options.Pusher(this.options.key, this.options);
         } else {
             this.pusher = new Pusher(this.options.key, this.options);
         }
