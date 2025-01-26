4b810a6cf4539c938f323eaaba5c279944cc4d2d
Sander Muller <github@scode.nl>
2025-01-26 14:57:12 +0100

    Fix TS error
    node_modules/laravel-echo/src/util/index.ts:5:13 - error TS18046: 'err' is of type 'unknown'.

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,9 +1,14 @@
-function isConstructor(obj: any): obj is new (...args: any[]) => any {
-    try {
-        new obj();
-    } catch (err) {
-        if (err.message.includes('is not a constructor')) return false;
+function isConstructor(obj: unknown): obj is new (...args: any[]) => any {
+    if (typeof obj !== 'function') {
+        return false;
     }
+
+    try {
+        Reflect.construct(String, [], obj);
+    } catch (err) {
+        return false;
+    }
+
     return true;
 }
 
