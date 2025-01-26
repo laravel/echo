8e93b0e58682a0d3ed3f7d5a7fe9a56323458a10
Sander Muller <github@scode.nl>
2025-01-26 15:18:19 +0100

    Update index.ts

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,12 +1,10 @@
 function isConstructor(obj: unknown): obj is new (...args: any[]) => any {
-    if (typeof obj !== 'function') {
-        return false;
-    }
-
     try {
-        Reflect.construct(String, [], obj);
+        new (obj as new (...args: any[]) => any)();
     } catch (err) {
-        return false;
+        if (err instanceof Error && err.message.includes('is not a constructor')) {
+            return false;
+        }
     }
 
     return true;
